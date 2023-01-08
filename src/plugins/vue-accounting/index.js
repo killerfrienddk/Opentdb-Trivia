8df78be221d3e9f7d35fa44c287391cd4d3d48b7
s <simonh1103@gmail.com>
2023-01-08 14:36:04 +0100

    adding dist

--- src/plugins/vue-accounting/index.js
+++
@@ -1,9 +0,0 @@
-export function VueAccounting(Vue) {
-    window.accounting = require("../../Lib/accounting");
-    Vue.prototype.$accounting = window.accounting;
-    // @ts-ignore
-    Vue.accounting = window.accounting;
-    // @ts-ignore
-    Vue.$accounting = window.accounting;
-}
-//# sourceMappingURL=index.js.map
